34c3518a96f6519f90be8ab8a4d3723120928dde
ferry-fauzan <fauzanferry123@gmail.com>
2020-10-06 15:41:58 +0700

    Menambahkan Kasus 1 Introducing

---
+++ Novice/01-01/Kasus/main.js
@@ -0,0 +1,15 @@
+let a = "Ferry Fauzan"
+console.log(a);
+document.write("Nama: " + a);
+
+let b = "Semarang"
+console.log("Asal Kota: " + b);
+document.write("Asal Kota: " + b);
+
+let c = "Front End"
+console.log("Kelas yang dipilih: " + c);
+document.write("Kelas yang dipilih: " + c);
+
+let d = "saya menyukai hal tentang web developer"
+console.log("Alasan pilih kelas " + c + " karena " + d);
+document.write("Alasan pilih kelas " + c + " karena " + d);
