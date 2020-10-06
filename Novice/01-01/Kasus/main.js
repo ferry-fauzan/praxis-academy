3a87fd24a953ee862d28617246611e15e477bfe8
ferry-fauzan <fauzanferry123@gmail.com>
2020-10-06 16:36:42 +0700

    Merapikan Baris js

--- Novice/01-01/Kasus/main.js
+++ Novice/01-01/Kasus/main.js
@@ -1,15 +1,15 @@
 let a = "Ferry Fauzan"
 console.log(a);
-document.write("Nama: " + a);
+document.write("Nama " + a);
 
 let b = "Semarang"
 console.log("Asal Kota: " + b);
-document.write("Asal Kota: " + b);
+document.write(". Saya berasal dari Kota " + b);
 
 let c = "Front End"
 console.log("Kelas yang dipilih: " + c);
-document.write("Kelas yang dipilih: " + c);
+document.write(". Kelas yang saya pilih " + c);
 
 let d = "saya menyukai hal tentang web developer"
 console.log("Alasan pilih kelas " + c + " karena " + d);
-document.write("Alasan pilih kelas " + c + " karena " + d);
+document.write(". Alasan memilih kelas " + c + " karena " + d);
