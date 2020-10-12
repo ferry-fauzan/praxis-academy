abd3d6724afa00dffd4f9c259e637e18d732a503
ferry-fauzan <fauzanferry123@gmail.com>
2020-10-12 12:57:04 +0700

    Refresh Folder

---
+++ Novice/Kasus/Kasus 01-03/Kasus A/kasus1.js
@@ -0,0 +1,8 @@
+let familyTree = (a, b, c, d) => {
+    document.write("Hi my name is " + a + " i like " + b + " i come from " + c + " ,nice to meet u " + d + "<br>")
+}
+
+
+familyTree("Fauzan", "Running", "Semarang", "Sekar")
+familyTree("Rashif", "Football", "Bandung", "Silla")
+familyTree("Raka", "Reading", "Jakarta", "Gadis")
---
+++ Novice/Kasus/Kasus 01-03/Kasus B/kasus2.js
@@ -0,0 +1,8 @@
+function cattyAge(a) {
+    let b = a * 7
+    document.write("Hai Catty Your age is " + b + "<br>")
+}
+
+cattyAge(2)
+cattyAge(4)
+cattyAge(8)
