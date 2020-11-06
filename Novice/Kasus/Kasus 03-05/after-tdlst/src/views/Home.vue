f1abbbdd9b121ddf65ec48da91b90ee201c532a5
ferry-fauzan <fauzanferry123@gmail.com>
2020-11-06 21:57:36 +0700

    AXIOS-API

--- Novice/Kasus/Kasus 03-05/after-tdlst/src/views/Home.vue
+++ Novice/Kasus/Kasus 03-05/after-tdlst/src/views/Home.vue
@@ -9,9 +9,11 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
+
   // components: {
   //   HelloWorld
   // }
--- Novice/Kasus/Kasus 03-05/after-tdlst/src/views/third.vue
+++ Novice/Kasus/Kasus 03-05/after-tdlst/src/views/third.vue
@@ -1,36 +1,132 @@
 <template>
 <div>
-    <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-col
-        v-for="n in 3"
-        :key="n"
-        cols="12"
-        sm="4"
+      <!-- <v-text-field
+        label="Input Your Text"
+        :rules="rules"
+        hide-details="auto"
+      ></v-text-field> -->
+
+       <v-row justify="center" align="center">
+        <!-- <v-col>
+          <v-btn @click="tampilkan" color="primary"> Call API </v-btn>
+        </v-col> -->
+        <template>
+          <v-btn @click="tampilkan" block>
+             Call API
+          </v-btn>
+        </template>
+      </v-row>
+
+      <v-list>
+      <v-list-item
+        v-for="pengguna in paraPengguna"
+        :key="pengguna.id"
       >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
+        <v-list-item-content>
+          <!-- <v-list-item-title >{{pengguna.website}}</v-list-item-title> -->
+           <v-list-item-title >Name: {{pengguna.title}} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+
+  <!-- <h1>{{$store.state.numb}}</h1>
+  <button @click="addNumbs">Klik Cuy</button> -->
+  <!-- <v-container fluid> -->
+    <!-- <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      hide-default-footer
+    > -->
+      <!-- <template v-slot:header>
+        <v-toolbar
+          class="mb-2"
+          color="indigo darken-5"
+          dark
+          flat
         >
-          One of three columns
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-  <h1>{{$store.state.numb}}</h1>
-  <button @click="addNumbs">Klik Cuy</button>
+          <v-toolbar-title>This is a header</v-toolbar-title>
+        </v-toolbar>
+         </template> -->
+         <!-- <v-list dense>
+                <v-list-item>
+                  <v-list-item-content>Calories:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.calories }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Fat:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.fat }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Carbs:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.carbs }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Protein:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.protein }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Sodium:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.sodium }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Calcium:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.calcium }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Iron:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ item.iron }}
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list> -->
+    <!-- </v-data-iterator> -->
+    <!-- </v-container> -->
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    methods:{
-        addNumbs:function(){
-            this.$store.commit("addNumb")
-        }
+  data: function(){
+    return{
+      paraPengguna: [],
     }
-}
+  },
+    methods:{
+        // addNumbs:function(){
+        //     this.$store.commit("addNumb")
+        // }
+        tampilkan: async function(){
+          let isi= await axios.get('http://192.168.1.26:3000/tasks');
+          let simpan= await isi.data;
+          this.paraPengguna = simpan;
+          console.log(this.paraPengguna)
+        }
+    },
+    created: function(){
+      this.tampilkan();
+    }
+};
 </script>
 
 <style>
